516712d2409cdfba9bef7251df78eca85e88065d
mandla mthethwa <hzmandla06@gmail.com>
2023-11-28 14:37:26 +0200

    added name('clients.index')

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,15 @@ Auth::routes();
 Route::resource('categories', clientController::class);
 
 //index
-Route::get('/', [ClientController::class, 'index']);
+Route::get('/', [ClientController::class, 'index'])->name('clients.index');
 
 //create
 Route::get('/clients/create', [ClientController::class,'create'])->name('clients.create');
 Route::post('/clients', [ClientController::class,'store'])->name('clients.store');
 
 //edit
-Route::get('/clients/{client}/edit', [clientController::class, 'edit'])->name('clients.edit');
-Route::put('/clients/{client}', [clientController::class, 'update'])->name('clients.update');
+Route::get('clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+Route::put('clients/{client}', [ClientController::class, 'update'])->name('clients.update');
 
-
-
-
+//delete
+Route::delete('clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
