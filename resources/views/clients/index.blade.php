c350f40397fa6b4236c46229d4263e265d9e4c69
mandla mthethwa <hzmandla06@gmail.com>
2023-11-28 11:02:00 +0200

    add index / homepage with table

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Client List</div>
+                    <div class="card-body">
+                        @if(session('success'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>ID Number</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($clients as $client)
+                                    <tr>
+                                        <td>{{ $client->id_number }}</td>
+                                        <td>{{ $client->first_name }}</td>
+                                        <td>{{ $client->last_name }}</td>
+                                        <td>{{ $client->email }}</td>
+                                        <td>{{ $client->phone }}</td>
+                                        <td>{{ $client->status }}</td>
+                                        <td>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
