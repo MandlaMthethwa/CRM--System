f80a2651e31b8984ecbcb8a77e86ac50ba728944
mandla mthethwa <hzmandla06@gmail.com>
2023-11-28 11:58:35 +0200

    updated edit view and web.php

---
+++ resources/views/clients/edit.blade.php
@@ -0,0 +1,59 @@
+<!-- resources/views/clients/edit.blade.php -->
+
+@extends('layouts.app') 
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Update Client details</div>
+
+                    <div class="card-body">
+                        @if(session('success'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('clients.update', $client->id) }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label for="id_number">ID Number</label>
+                                <input type="number" name="id_number" class="form-control" value="{{ $client->id_number }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="first_name">First Name</label>
+                                <input type="text" name="first_name" class="form-control" value="{{ $client->first_name }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="last_name">Last Name</label>
+                                <input type="text" name="last_name" class="form-control" value="{{ $client->last_name }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="form-control" value="{{ $client->email }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">Phone</label>
+                                <input type="text" name="phone" class="form-control" value="{{ $client->phone }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="status">Status</label>
+                                <textarea name="status" class="form-control" required>{{ $client->status }}</textarea>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Update Client</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ use App\Http\Controllers\ClientController;
 Auth::routes();
 //index
 Route::get('/', [ClientController::class, 'index']);
+
 //create
 Route::get('/clients/create', [ClientController::class,'create'])->name('clients.create');
 Route::post('/clients', [ClientController::class,'store'])->name('clients.store');
+
 //edit
 Route::get('/clients/{client}/edit', [clientController::class, 'edit'])->name('clients.edit');
 Route::put('/clients/{client}', [clientController::class, 'update'])->name('clients.update');
