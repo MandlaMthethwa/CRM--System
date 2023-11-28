a90869cb58dc37e04b3690ae0862525a3c852530
mandla mthethwa <hzmandla06@gmail.com>
2023-11-28 09:04:03 +0200

    create client controller

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Client;
+
+
+class ClientController extends Controller
+{
+    public function index(){
+        $clients = Client::all();
+        return view('clients.index', compact('clients'));
+    }
+}
