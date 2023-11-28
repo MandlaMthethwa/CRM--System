6225ef6fc42afa1e539d1e637f2035d7abf23fc6
mandla mthethwa <hzmandla06@gmail.com>
2023-11-28 11:28:18 +0200

    edit controller function

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -36,5 +36,30 @@ class ClientController extends Controller
     $clients->save();
     return redirect('clients/');
 }
+public function edit(Client $client)
+{
+    return view('clients.edit', compact('client'));
+}
+
+public function update(Request $request, Client $client)
+{
+    $validatedData = $request->validate([
+        'id_number' => 'required|numeric|unique:your_table_name,id_number,' . $client->id . '|max:9999999999999',
+        'first_name' => 'required|string',
+        'last_name' => 'required|string',
+        'email' => 'required|email|unique:your_table_name,email,' . $client->id,
+        'phone' => 'required|string',
+        'status' => 'required|string',
+    ]);
+
+    $client->id_number = $validatedData['id_number'];
+    $client->first_name = $validatedData['first_name'];
+    $client->last_name = $validatedData['last_name'];
+    $client->email = $validatedData['email'];
+    $client->phone = $validatedData['phone'];
+    $client->status = $validatedData['status'];
+
+    return redirect('clients/');
+}
 
 }
